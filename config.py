66535f1ffa791b367778ee49444c03f5b290046d
Ihor <igor.l@myualab.com>
2023-03-31 16:21:18 +0300

    chat_id to int

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config:
     """
 
     TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-    TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+    TELEGRAM_CHAT_ID = int(os.getenv('TELEGRAM_CHAT_ID'))
 
     POSTGRES = {
         'database': os.getenv('POSTGRES_NAME'),
