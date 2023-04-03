2ce195642de3ebb6062e4c38f2cef962e4edc22a
Ihor <igor.l@myualab.com>
2023-04-03 14:07:24 +0300

    fix migrations

--- migrations/versions/014_alter_table_balancing_reporsts_add_column_env.sql
+++ migrations/versions/014_alter_table_balancing_reporsts_add_column_env.sql
@@ -2,6 +2,6 @@ BEGIN;
 
 LOCK balancing_reports IN ROW EXCLUSIVE MODE NOWAIT;
 
-ALTER TABLE balancing_reports add column env varchar(64) NOT NULL;
+ALTER TABLE balancing_reports add column env varchar(64) NOT NULL default 'DEFAULT TEST';
 
 COMMIT;
--- migrations/versions/015_alter_table_balance_check_add_column_env.sql
+++ migrations/versions/015_alter_table_balance_check_add_column_env.sql
@@ -2,6 +2,6 @@ BEGIN;
 
 LOCK balance_check IN ROW EXCLUSIVE MODE NOWAIT;
 
-ALTER TABLE balance_check add column env varchar(64) NOT NULL;
+ALTER TABLE balance_check add column env varchar(64) NOT NULL default 'DEFAULT TEST';
 
 COMMIT;
