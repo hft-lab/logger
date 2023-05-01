9cd2382a6035989e80c10db30ae3466f6c1dc9f7
Ihor <igor.l@myualab.com>
2023-05-01 12:07:57 +0300

    fix migration

--- migrations/versions/023_alter_table_deals_reports_add_coin.sql
+++ migrations/versions/023_alter_table_deals_reports_add_coin.sql
@@ -2,4 +2,4 @@ ALTER TABLE deals_reports
 ADD COLUMN coin char(16),
 ADD COLUMN sell_order_id char(64),
 ADD COLUMN buy_order_id char(64),
-ADD COLUMN date_utc datetime;
+ADD COLUMN date_utc timestamptz;
