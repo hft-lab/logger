db998cde6471ef66e64e0b3af91c512575a874f7
Ihor <igor.l@myualab.com>
2023-07-05 11:23:54 +0300

    fix migration

--- migrations/versions/044_alter_table_arbitage_possibilities_add_column.sql
+++ migrations/versions/044_alter_table_arbitage_possibilities_add_column.sql
@@ -1,7 +1,7 @@
 BEGIN;
 
-LOCK arbitage_possibilities IN ROW EXCLUSIVE MODE NOWAIT;
+LOCK arbitrage_possibilities IN ROW EXCLUSIVE MODE NOWAIT;
 
-ALTER TABLE arbitage_possibilities add column bot_launch_id varchar(64);
+ALTER TABLE arbitrage_possibilities add column bot_launch_id varchar(64);
 
 COMMIT;
